ef7e115a05eb1140ad7b6556a06aac4d9026e6e5
Shamimgardobuya <shamimobuya@gmail.com>
2022-07-12 15:10:07 +0300

    assignment for classes and objects

---
+++ assignment.js
@@ -0,0 +1,49 @@
+class Fruits{
+    constructor(name,quantity){
+        this.name=name
+        this.quantity=quantity
+
+    }
+}
+let item1=new Fruits("Book",6);   //object
+function pricing(){           //function for pricing
+    let x={"Orange":40,"Book":80};   //object for price
+    if (item1.name in x){             //check if property of object in pricing object
+        // for (const val in x){
+            let pricing= x[item1.name]*item1.quantity   //multiply for the exact property//will calculate for the specific price,no loops  
+            console.log(`${pricing} is the price of ${item1.name}`);                     
+
+        //  }  
+
+    }
+    else{
+        console.log("Item not found");
+    }
+      //for price of each item 
+    //multiplied the attribute with value of object
+                                 //console for price.
+}
+pricing()
+// create a class,takes in fruit,quantity
+//create a this holding type of fruit with price
+//create instance of class
+//create a method for multiplying the value
+class KioskCalc{
+    constructor(fruitb,quantityb){
+        this.fruitb=fruitb
+        this.quantityb=quantityb
+        this.fruitsPricelist={"orange": 30, "mango": 15, "avocado": 40}
+    }
+
+}
+let customer1={
+    fruitb:"Orange",
+    quantityb:2,
+    pricee:function getTotalCost(){
+              let result= customer1.quantityb*${this.fruitsPricelist[customer1.fruitb]}
+              console.log(result);
+
+    }
+};
+console.log(customer1.pricee());
+
